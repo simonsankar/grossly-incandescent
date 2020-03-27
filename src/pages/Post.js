8855f16483a4356ca700ae59b2badc33ce6c6f5d
Simon Sankar <simon.sankar@wepala.com>
2020-03-26 22:10:17 -0400

    Refactored plugin imports for prismjs

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -3,15 +3,15 @@ import ReactMarkdown from "react-markdown";
 // PrismJS
 import PrismJS from "prismjs";
 // Languages
-import JSX from "prismjs/components/prism-jsx.min";
+import "prismjs/components/prism-jsx.min";
 // Plugins
-import Toolbar from "prismjs/plugins/toolbar/prism-toolbar.min.js";
+import "prismjs/plugins/toolbar/prism-toolbar.min.js";
 import "prismjs/plugins/toolbar/prism-toolbar.css";
-import ShowLanguage from "prismjs/plugins/show-language/prism-show-language.min.js";
-import Keywords from "prismjs/plugins/highlight-keywords/prism-highlight-keywords.min.js";
-import Copy from "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js";
-import InlineColor from "prismjs/plugins/inline-color/prism-inline-color.min.js";
-import LineNumbers from "prismjs/plugins/line-numbers/prism-line-numbers.min.js";
+import "prismjs/plugins/show-language/prism-show-language.min.js";
+import "prismjs/plugins/highlight-keywords/prism-highlight-keywords.min.js";
+import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js";
+import "prismjs/plugins/inline-color/prism-inline-color.min.js";
+import "prismjs/plugins/line-numbers/prism-line-numbers.min.js";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 
 import reduxMD from "../md/redux.md";
