5304519e58584d4777e150abeb6754e1f1727656
Simon Sankar <simonlfcsuarez7@gmail.com>
2020-04-23 20:50:08 -0400

    Hmmm

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -20,54 +20,56 @@ const Login = (props) => {
   };
 
   return (
-    <Row className="login" align="middle" justify="center">
-      <Col xs={22} sm={20} md={18} lg={12}>
-        <Card
-          className="login__form"
-          title="Login"
-          extra={
-            user.loading
-              ? "Loading..."
-              : user.error
-              ? `${user.error.message}`
-              : "Are you sure you're Simon?"
-          }
-        >
-          <Form
-            name="basic"
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
+    <div className="login">
+      <Row align="middle" justify="center">
+        <Col xs={22} sm={20} md={18} lg={12}>
+          <Card
+            className="login__form"
+            title="Login"
+            extra={
+              user.loading
+                ? "Loading..."
+                : user.error
+                ? `${user.error.message}`
+                : "Are you sure you're Simon?"
+            }
           >
-            <Form.Item
-              label="Email"
-              name="email"
-              rules={[
-                { required: true, message: "Please input your username!" },
-              ]}
+            <Form
+              name="basic"
+              initialValues={{ remember: true }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
             >
-              <Input />
-            </Form.Item>
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                  { required: true, message: "Please input your username!" },
+                ]}
+              >
+                <Input />
+              </Form.Item>
 
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[
-                { required: true, message: "Please input your password!" },
-              ]}
-            >
-              <Input.Password />
-            </Form.Item>
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  { required: true, message: "Please input your password!" },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
 
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-        </Card>
-      </Col>
-    </Row>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
